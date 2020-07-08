b9cd68c569a8a4efeec29d1b481b66dbaf0ff21a
dev-akshat <akshatt004.at@gmail.com>
2020-07-08 18:38:18 +0530

    Adding basic signUp & signIn page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 import {HomeComponent} from './pages/home/home.component';
-import {LoginComponent} from './pages/login/login.component';
 import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {SignUpComponent} from './pages/sign-up/sign-up.component';
+import {SignInComponent} from './pages/sign-in/sign-in.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'signIn', component: SignInComponent},
+  {path: 'signUp', component: SignUpComponent},
   {path: '404', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/404'},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,20 @@ import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {NavbarComponent} from './pages/partials/navbar/navbar.component';
 import {HomeComponent} from './pages/home/home.component';
-import {LoginComponent} from './pages/login/login.component';
 import {FooterComponent} from './pages/partials/footer/footer.component';
 import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {SignUpComponent} from './pages/sign-up/sign-up.component';
+import {SignInComponent} from './pages/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    LoginComponent,
     FooterComponent,
     PageNotFoundComponent,
+    SignUpComponent,
+    SignInComponent,
   ],
   imports: [
     BrowserModule,
