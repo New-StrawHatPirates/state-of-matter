86b7cff2b5edeee099bd2d859cb7a694495a9dff
dev-akshat <akshatt004.at@gmail.com>
2020-07-22 22:52:59 +0530

    Adding anime page & carousel list and updating carousel.

--- src/app/pages/partials/carousel-list/carousel-list.component.ts
+++ src/app/pages/partials/carousel-list/carousel-list.component.ts
@@ -6,17 +6,19 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./carousel-list.component.css']
 })
 export class CarouselListComponent implements OnInit {
-
   constructor() {
   }
 
-  urlBase = '../../../assets/img/anime/';
+  urlBase = '../../../assets/img/nature/image';
   imgUrl = [
     this.urlBase + '1.jpg',
     this.urlBase + '2.jpg',
     this.urlBase + '3.jpg',
+    this.urlBase + '4.jpg',
+    this.urlBase + '5.jpg',
+    this.urlBase + '4.jpg',
+    this.urlBase + '5.jpg',
   ];
-
 
   ngOnInit(): void {
   }
